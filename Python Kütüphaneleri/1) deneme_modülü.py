a1fd6444fd829d833594183026069c17be355c26
Ozgur Can Arıcan <56658730+ozgurcanarican@users.noreply.github.com>
2024-03-08 21:22:36 +0300

    Add files via upload

---
+++ Python Kütüphaneleri/1) deneme_modülü.py
@@ -0,0 +1,18 @@
+def giris_mesaji():
+    print("Merhaba Dünya")
+
+def toplama(x, y):
+    toplam = x + y
+    return toplam
+
+def cikarma(x, y):
+    fark = x - y
+    return fark
+
+def carpma(x, y):
+    carpim = x * y
+    return carpim
+
+def bolme(x, y):
+    bolum = x / y
+    return bolum
