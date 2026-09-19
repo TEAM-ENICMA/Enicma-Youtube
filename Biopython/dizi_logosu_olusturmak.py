f9cb1ace67ad03215654847da363f256ba5abdd4
Ozgur Can Arıcan <56658730+ozgurcanarican@users.noreply.github.com>
2026-09-19 16:38:48 +0300

    Add files via upload

---
+++ Biopython/dizi_logosu_olusturmak.py
@@ -0,0 +1,17 @@
+from Bio.Seq import Seq
+from Bio import motifs
+
+ornekler = [
+    Seq("TACAA"),
+    Seq("TACGC"),
+    Seq("TACAC"),
+    Seq("TACCC"),
+    Seq("AACCC"),
+    Seq("AATGC"),
+    Seq("AATGC")
+    ]
+
+m = motifs.create(ornekler)
+print(m)
+
+m.weblogo("motif.png")
